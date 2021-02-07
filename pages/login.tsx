264c3e30e908d403d7f90c06f9b4745601c50fe3
ykpark <read0more@gmail.com>
2021-02-07 19:44:55 +0900

    Fix: 불필요한 페이지 이동 안하게 변경

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
+import Home from ".";
+import { GetStaticProps } from "next";
 
-export default function Login() {
+export default function Login({ loginUrl }) {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
     if (router.query.jwt) {
+      window.history.pushState({}, null, "/");
       localStorage.setItem("jwt", router.query.jwt as string);
+      setLoading(false);
     }
-    router.push("/");
-  });
+  }, [router]);
 
-  return <LoadingSpinner />;
+  return loading ? <LoadingSpinner /> : <Home loginUrl={loginUrl} />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      loginUrl: process.env.GITHUB_LOGIN_URL,
+    },
+  };
+};
